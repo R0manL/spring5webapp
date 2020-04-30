0bd577cad7f09116998396114ad36ed268806544
roman <roman.liubun@skelia.com.ua>
2020-04-30 13:39:53 +0300

    initial commit

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -9,4 +9,9 @@ public class Spring5webappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
+
+	// To evaluate H2 DB go to http://localhost:8080/h2-console/
+	// set DB path as > jdbc:h2:mem:testdb
+
+
 }
